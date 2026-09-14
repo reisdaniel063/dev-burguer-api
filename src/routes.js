649b68ef46ac8d8fa00187bb75b8fad0f7d52622
Daniel Vitor <reisdaniel063@gmail.com>
2026-09-14 17:57:00 -0300

    Adiciona middleware de autenticacao JWT nas rotas de produtos

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,15 @@ import ProductController from './app/controllers/ProductController.js';
 import SessionController from './app/controllers/SessionController.js';
 import UserController from './app/controllers/UserController.js';
 import multerConfig from './config/multer.cjs';
+import authMiddleware from './middlewares/auth.js';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index);
 
