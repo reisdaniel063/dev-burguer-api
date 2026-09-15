07aa564095fcb99829887f83afc62833cefa57d9
Daniel Vitor <reisdaniel063@gmail.com>
2026-09-15 15:14:30 -0300

    Criando a estrutura de Categories

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 import { Sequelize } from 'sequelize';
+import Category from '../app/models/Category.js';
 import Product from '../app/models/Product.js';
 import User from '../app/models/User.js';
 import databaseConfig from '../config/database.cjs';
 
-const models = [User, Product];
+const models = [User, Product, Category];
 
 class Database {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import CategoryController from './app/controllers/CategoryController.js';
 import ProductController from './app/controllers/ProductController.js';
 import SessionController from './app/controllers/SessionController.js';
 import UserController from './app/controllers/UserController.js';
@@ -15,5 +16,8 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index);
+
+routes.post('/categories', CategoryController.store);
+routes.get('/categories', CategoryController.index);
 
 export default routes;
